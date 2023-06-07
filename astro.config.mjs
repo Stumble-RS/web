faac05141bc86475e4d6b4eef4e18fb3eede0bbb
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-06-07 23:19:22 +0530

    Add waitlist email option

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 
+import vercelEdge from '@astrojs/vercel/edge';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+  integrations: [compress()],
+  output: "server",
+  adapter: vercelEdge()
 });
