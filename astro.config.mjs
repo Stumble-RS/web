7efa3a386dae5e8c405a67835dbc6b6ff792cd46
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-10-12 03:47:26 +0530

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     speedInsights: {
-      enabled: true,
+      enabled: true
     }
   }),
-  integrations: [compress()],
+  integrations: [compress(), sitemap()]
 });
