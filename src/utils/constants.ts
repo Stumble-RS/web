df6390be82f6cbaf0b0c8e1b3a993e5cf0e377ff
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-10-24 01:12:54 +0530

    Add college ambassador program form, improve title & add shuffle to cities

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,9 +6,9 @@ export const PAGE_MODE = {
 }
 
 export const FEATURED_PLACES : FeaturedPlace[] = [
-  { name: "Bangalore", route: "bangalore", tagline: "The Silicon Valley of India" },
-  {name: "Delhi", route: "delhi", tagline: "The Capital of India"},
-  {name: "Mumbai", route: "mumbai", tagline: "The City of Dreams"},
+  { name: "Bangalore", route: "bangalore", tagline: "The Garden City, Where Innovation Blossoms." },
+  {name: "Delhi", route: "delhi", tagline: "A Historical Tapestry of Modern Lifestyles."},
+  {name: "Mumbai", route: "mumbai", tagline: "The City of Dreams, Where Every Dreamer Thrives"},
 ];
 
 export const FEATURED_PLACE_ROUTE_TO_OBJ = FEATURED_PLACES.reduce((acc : {[city: string]: FeaturedPlace}, curr: FeaturedPlace) => {
